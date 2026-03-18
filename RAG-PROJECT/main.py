f7c87f0dd95273872103f8f1084f578941235773
Md Sonu <mdsonu22943@gmail.com>
2026-03-18 15:48:54 +0530

    done with document loader

--- RAG-PROJECT/main.py
+++ RAG-PROJECT/main.py
@@ -1,19 +1,22 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
 data = TextLoader("RAG-PROJECT/document-loaders/notes.txt")
+pdfdata = PyPDFLoader("RAG-PROJECT/document-loaders/GRU.pdf")
 
 docs=data.load()
+pdfdocs=pdfdata.load()
 
-template=ChatPromptTemplate.from_messages([("system", "You are a helpful assistant and summarize the given text"),("human","{data}")])
+template=ChatPromptTemplate.from_messages([("system", "You are a helpful assistant and summarize the given text"),("human","{pdfdocs}")])
 
 model = ChatMistralAI(model="mistral-small-2506")
 
-prompt=template.format_messages(data=docs[0].page_content)
+prompt=template.format_messages(pdfdocs=pdfdocs[0].page_content)
 
 result = model.invoke(prompt)
 print(result.content)
