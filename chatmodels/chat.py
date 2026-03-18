767bf3298841a5bb50050469eeb03afaa4ce9047
Md Sonu <mdsonu22943@gmail.com>
2026-03-18 14:48:10 +0530

    chatmodel done

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -6,6 +6,6 @@ from langchain_mistralai import ChatMistralAI
 
 model = ChatMistralAI(model = "mistral-small-2506",temperature=0.9)
 
-response = model.invoke("write a poem on AI")
+response = model.invoke("hi")
 
 print(response.content)
