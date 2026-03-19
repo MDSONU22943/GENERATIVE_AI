5c8e58a03cbfe051bd2826c85b8f887d2f665d6e
Md Sonu <mdsonu22943@gmail.com>
2026-03-19 15:04:11 +0530

    done with character splitter

--- RAG-PROJECT/document-loaders/test.py
+++ RAG-PROJECT/document-loaders/test.py
@@ -1,8 +1,16 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
 
 data=TextLoader("RAG-PROJECT/document-loaders/notes.txt")
 
 docs=data.load()
-# print(docs)
-# print(len(docs))
-print(docs[0].page_content)
+
+splits = splitter.split_documents(docs)
+# print(splits)
+# print(len(splits))
+for i in splits:
+    print("********")
+    print(i.page_content)
+    print("-----------")
